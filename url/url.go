db6990bb4c6e63a24507b758b3ed0415b10b2c8f
liguangmin <li-h-8@qq.com>
2023-02-08 19:33:05 +0800

    php2go url function

--- url/url.go
+++ url/url.go
@@ -1,15 +1,42 @@
 package url
 
-import "net/url"
+import (
+	netUrl "net/url"
+	"strings"
+)
+
+// ParseUrl parse url_string
+// php parse_url
+func ParseUrl(rawURL string) (*netUrl.URL, error) {
+	return netUrl.Parse(rawURL)
+}
 
 // UrlEncode url-encode string
 // php urlencode
 func UrlEncode(str string) string {
-	return url.QueryEscape(str)
+	return netUrl.QueryEscape(str)
 }
 
 // UrlDecode url-decode string
 // php urldecode
 func UrlDecode(str string) (string, error) {
-	return url.QueryUnescape(str)
+	return netUrl.QueryUnescape(str)
+}
+
+// RawUrlEncode raw url-encode string
+// php rawurlencode
+func RawUrlEncode(str string) string {
+	return strings.Replace(netUrl.QueryEscape(str), "+", "%20", -1)
+}
+
+// RawUrlDecode raw url-decode string
+// php rawurldecode
+func RawUrlDecode(str string) (string, error) {
+	return netUrl.QueryUnescape(strings.Replace(str, "%20", "+", -1))
+}
+
+// HttpBuildQuery http build query
+// php http_build_query
+func HttpBuildQuery(queryData netUrl.Values) string {
+	return queryData.Encode()
 }
--- url/url_test.go
+++ url/url_test.go
@@ -1,15 +1,29 @@
 package url
 
 import (
+	"encoding/json"
 	"fmt"
 	"testing"
 )
 
 func TestUrl(t *testing.T) {
-	str := "你好啊~"
+	urlStr := "https://www.example.com/path?googleguy=googley"
+	urlData, err := ParseUrl(urlStr)
+	urlDataByte, _ := json.Marshal(urlData)
+	fmt.Println("ParseUrl: ", string(urlDataByte), ", Error: ", err)
 
-	urlStr := UrlEncode(str)
-	fmt.Println("UrlEncode: ", urlStr)
-	uStr, err := UrlDecode(urlStr)
+	str := "你好 啊~ "
+	urlEnStr := UrlEncode(str)
+	fmt.Println("UrlEncode: ", urlEnStr)
+	uStr, err := UrlDecode(urlEnStr)
 	fmt.Println("UrlDecode: ", uStr, ", Error: ", err)
+
+	urlREnStr := RawUrlEncode(str)
+	fmt.Println("RawUrlEncode: ", urlREnStr)
+	uRStr, err := RawUrlDecode(urlREnStr)
+	fmt.Println("RawUrlDecode: ", uRStr, ", Error: ", err)
+
+	queryData := map[string][]string{}
+	_ = json.Unmarshal(urlDataByte, &queryData)
+	fmt.Println(HttpBuildQuery(queryData))
 }
