7e83b88a146cfc87ec5d417bd9faffc743fc8b7b
liguangmin <li-h-8@qq.com>
2023-02-09 15:36:57 +0800

    php2go string

--- string/string.go
+++ string/string.go
@@ -1,1 +1,62 @@
 package string
+
+import (
+	"strings"
+	"unicode/utf8"
+)
+
+// StrLen string length
+// php strlen
+func StrLen(s string) int {
+	return len(s)
+}
+
+// MbStrLen string for utf8
+// php mb_strlen
+func MbStrLen(s string) int {
+	return utf8.RuneCountInString(s)
+}
+
+// SubstrCount counts the number of substr in s
+// php substr_count
+func SubstrCount(s, substr string) int {
+	return strings.Count(s, substr)
+}
+
+// Substr get substr
+// php substr
+func Substr(s string, start int, length int) string {
+	return s[start : start+length]
+}
+
+// MbSubstr get substr for utf8
+// php mb_substr
+func MbSubstr(s string, start int, length int) string {
+	strRune := []rune(s)
+	return string(strRune[start : start+length])
+}
+
+// StrPos get first index of substr in s, from "start" index
+// php strpos
+func StrPos(s, substr string, start int) int {
+	return strings.Index(s[start:], substr)
+}
+
+// StrRPos get last index of substr in s, from "start" index
+// php strrpos
+func StrRPos(s, substr string, start int) int {
+	return strings.LastIndex(s[start:], substr)
+}
+
+// StrSplit slices s into all substrings separated by sep
+// php str_split
+func StrSplit(s, sep string) []string {
+	return strings.Split(s, sep)
+}
+
+// UCFirst toUpper the first letter
+// php ucfirst
+func UCFirst(s string) string {
+	firstChart := strings.ToUpper(s[0:1])
+	return firstChart + s[1:]
+}
--- string/string_test.go
+++ string/string_test.go
@@ -1,1 +1,24 @@
 package string
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestString(t *testing.T) {
+	str := "中国88-china-88"
+	fmt.Println("StrLen: ", StrLen(str))
+	fmt.Println("MbStrLen: ", MbStrLen(str))
+
+	haystack := "I love Shanghai. Shanghai is the biggest city in china."
+	needle := "Shanghai"
+	fmt.Println("SubstrCount: ", SubstrCount(haystack, needle))
+
+	fmt.Println("Substr: ", Substr(str, 1, 2))
+	fmt.Println("MbSubstr: ", MbSubstr(str, 1, 2))
+
+	fmt.Println("StrPos: ", StrPos(str, "-", 2))
+	fmt.Println("StrRPos: ", StrRPos(str, "-", 9))
+
+	fmt.Println("UCFirst: ", UCFirst("abcdefg"))
+}
