1612a509c5f9d03eb4c0584e17e175e6352e707f
liguangmin <li-h-8@qq.com>
2023-02-09 14:42:47 +0800

    php2go math

--- math/math.go
+++ math/math.go
@@ -1,1 +1,72 @@
 package math
+
+import (
+	"math"
+	"strconv"
+)
+
+// Abs absolute value
+// php abs
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+
+// Round number round
+// php round
+func Round(x float64) int64 {
+	return int64(math.Floor(x + 0.5))
+}
+
+// Floor returns the greatest integer value less than or equal to x
+// php floor
+func Floor(x float64) float64 {
+	return math.Floor(x)
+}
+
+// Ceil returns the least integer value greater than or equal to x
+// php ceil
+func Ceil(x float64) float64 {
+	return math.Ceil(x)
+}
+
+// Max returns the max num in nums
+// php max
+func Max(nums ...float64) float64 {
+	if len(nums) < 2 {
+		panic("nums: the nums length is less than 2")
+	}
+
+	maxNum := nums[0]
+	for i := 1; i < len(nums); i++ {
+		maxNum = math.Max(maxNum, nums[i])
+	}
+
+	return maxNum
+}
+
+// Min returns the min num in nums
+// php min
+func Min(nums ...float64) float64 {
+	if len(nums) < 2 {
+		panic("nums: the nums length is less than 2")
+	}
+
+	minNum := nums[0]
+	for i := 1; i < len(nums); i++ {
+		minNum = math.Min(minNum, nums[i])
+	}
+
+	return minNum
+}
+
+// DecBin Decimal to binary
+// php decbin
+func DecBin(number int64) string {
+	return strconv.FormatInt(number, 2)
+}
+
+// DecHex Decimal to hexadecimal
+// php dechex
+func DecHex(number int64) string {
+	return strconv.FormatInt(number, 16)
+}
--- math/math_test.go
+++ math/math_test.go
@@ -1,1 +1,24 @@
 package math
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRand(t *testing.T) {
+	fmt.Println("Abs: ", Abs(-45.6))
+
+	fmt.Println("Round: ", Round(3.1), "  ", Round(3.6))
+
+	fmt.Println("Floor: ", Floor(3.1), "  ", Floor(3.6))
+
+	fmt.Println("Ceil: ", Ceil(3.1), "  ", Ceil(3.6))
+
+	fmt.Println("Max: ", Max(1, 2, 3.5, 4))
+
+	fmt.Println("Min: ", Min(1, 2, 3.5, 4))
+
+	fmt.Println("DecBin: ", DecBin(10))
+
+	fmt.Println("DecHex: ", DecHex(10))
+}
